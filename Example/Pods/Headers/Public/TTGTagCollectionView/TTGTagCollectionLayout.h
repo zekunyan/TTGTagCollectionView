35e2d7869c071e49a943397c5880de8816f8abbd
zekunyan <zekunyan@163.com>
2016-07-17 17:38:03 +0800

    Update example pod file.

---
+++ Example/Pods/Headers/Public/TTGTagCollectionView/TTGTagCollectionLayout.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/TTGTagCollectionLayout.h
--- Example/Pods/Target Support Files/Pods-TTGTagCollectionView_Example/Pods-TTGTagCollectionView_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TTGTagCollectionView_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TTGTagCollectionView_ExampleVersionString[];
-
